9ee3fb20a1418fce16192969a5f388646ea9e017
marcuspereiragithub <vinnycoast@gmail.com>
2018-01-01 16:34:07 -0200

    All introductions in only one place

---
+++ app/public/wp-content/themes/Bikcraft/inc/introducao.php
@@ -0,0 +1,7 @@
+<!-- INTRODUÇÃO -->
+<section class="introducao_interna interna_contato">
+    <div class="container">
+        <h1>Contato</h1>
+        <p>tire suas dúvidas com a gente</p>
+    </div>
+</section>
