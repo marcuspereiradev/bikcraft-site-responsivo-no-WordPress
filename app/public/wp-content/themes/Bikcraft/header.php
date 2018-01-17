6b0a2ea6618ca82adb46e5750aab92f0b60a2420
marcuspereiragithub <vinnycoast@gmail.com>
2018-01-16 22:25:12 -0200

    Improving SEO and adding some fields

--- app/public/wp-content/themes/Bikcraft/header.php
+++ app/public/wp-content/themes/Bikcraft/header.php
@@ -2,18 +2,18 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title><?php bloginfo('name'); ?></title>
-    <meta name="description" content="Compre a sua bicicleta personalizada na Bikcraft. Temos modelos Passeio, Retrô e Esporte.">
+    <title><?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('title_seo'); ?></title>
+    <meta name="description" content="<?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('description_seo'); ?>">
     
     <meta property="og:type" content="website"/>
-    <meta property="og:title" content="<?php bloginfo('name'); ?>"/>
-    <meta property="og:description" content="Compre a sua bicicleta personalizada na Bikcraft. Temos modelos Passeio, Retrô e Esporte."/>
-    <meta property="og:url" content="http://bikcraft.com"/>
-    <meta property="og:image" content="http://bikcraft.com/img/og-image.png"/>
+    <meta property="og:title" content="<?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('title_seo'); ?>"/>
+    <meta property="og:description" content="<?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('title_seo'); ?>"/>
+    <meta property="og:url" content="<?php bloginfo('url'); ?>"/>
+    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/og-image.png"/>
     
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 
     <script src="js/libs/modernizr.custom.45655.js"></script>
     <!-- INÍCIO WORDPRESS HEADER-->
